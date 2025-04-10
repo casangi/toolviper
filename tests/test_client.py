81a0c082744c4ee99b3c0417f3aca4ef86bbaf36
Jan-Willem <jwsteeb@gmail.com>
2025-04-10 11:51:41 -0400

    Inable tests.

--- tests/test_client.py
+++ tests/test_client.py
@@ -28,35 +28,35 @@ class TestToolViperClient:
         """teardown any state that was previously setup for all methods of the given class"""
         pass
 
-    # def test_client_spawn(self):
-    #     """
-    #     Run astrohack_local_client with N cores and with a memory_limit of M GB to create an instance of the
-    #     astrohack Dask client.
-    #     """
+    def test_client_spawn(self):
+        """
+        Run astrohack_local_client with N cores and with a memory_limit of M GB to create an instance of the
+        astrohack Dask client.
+        """
 
-    #     log_params = {
-    #         "log_level": "DEBUG",
-    #         "log_to_file": True,
-    #         "log_file": "toolviper_log_file",
-    #     }
+        log_params = {
+            "log_level": "DEBUG",
+            "log_to_file": True,
+            "log_file": "toolviper_log_file",
+        }
 
-    #     path = pathlib.Path(".").cwd() / "dask_test_dir"
+        path = pathlib.Path(".").cwd() / "dask_test_dir"
 
-    #     client = local_client(
-    #         cores=2,
-    #         memory_limit="8GB",
-    #         dask_local_dir=str(path),
-    #         log_params=log_params,
-    #     )
+        client = local_client(
+            cores=2,
+            memory_limit="8GB",
+            dask_local_dir=str(path),
+            log_params=log_params,
+        )
 
-    #     try:
-    #         if distributed.Client.current() is None:
-    #             raise OSError
+        try:
+            if distributed.Client.current() is None:
+                raise OSError
 
-    #     except OSError:
-    #         assert False
+        except OSError:
+            assert False
 
-    #     client.shutdown()
+        client.shutdown()
 
     def test_client_get(self):
         """
@@ -73,84 +73,84 @@ class TestToolViperClient:
 
         client.shutdown()
 
-    # def test_cluster_get(self):
-    #     """
-    #     Test the get_client() function.
-    #     """
-    #     from toolviper.dask.client import get_cluster
+    def test_cluster_get(self):
+        """
+        Test the get_client() function.
+        """
+        from toolviper.dask.client import get_cluster
 
-    #     client = local_client(
-    #         cores=2,
-    #         memory_limit="4GB",
-    #     )
+        client = local_client(
+            cores=2,
+            memory_limit="4GB",
+        )
 
-    #     assert get_cluster() == distributed.Client.current().cluster
+        assert get_cluster() == distributed.Client.current().cluster
 
-    #     client.shutdown()
+        client.shutdown()
 
-    # def test_client_thread_info(self):
-    #     """
-    #     Test that thread_info() function returns the values that were set in the client instantiation.
-    #     """
+    def test_client_thread_info(self):
+        """
+        Test that thread_info() function returns the values that were set in the client instantiation.
+        """
 
-    #     client = local_client(
-    #         cores=2,
-    #         memory_limit="4GB",
-    #     )
+        client = local_client(
+            cores=2,
+            memory_limit="4GB",
+        )
 
-    #     memory_per_thread = -1
-    #     n_threads = 0
+        memory_per_thread = -1
+        n_threads = 0
 
-    #     # Not sure if this test is deterministic. The tests are done using github actions and, I am sure the container
-    #     # environment will change over time. Hopefully, the test pulls out the most consistently calculated case.
+        # Not sure if this test is deterministic. The tests are done using github actions and, I am sure the container
+        # environment will change over time. Hopefully, the test pulls out the most consistently calculated case.
 
-    #     worker_items = client.cluster.scheduler_info['workers'].items()
+        worker_items = client.cluster.scheduler_info['workers'].items()
 
-    #     for worker_name, worker in worker_items:
-    #         temp_memory_per_thread = (worker['memory_limit'] / worker['nthreads']) / (1024 ** 3)
-    #         n_threads = n_threads + worker['nthreads']
+        for worker_name, worker in worker_items:
+            temp_memory_per_thread = (worker['memory_limit'] / worker['nthreads']) / (1024 ** 3)
+            n_threads = n_threads + worker['nthreads']
 
-    #         if (memory_per_thread == -1) or (memory_per_thread > temp_memory_per_thread):
-    #             memory_per_thread = temp_memory_per_thread
+            if (memory_per_thread == -1) or (memory_per_thread > temp_memory_per_thread):
+                memory_per_thread = temp_memory_per_thread
 
-    #     assert client.thread_info() == {
-    #         "n_threads": 2,
-    #         "memory_per_thread": memory_per_thread
-    #     }
+        assert client.thread_info() == {
+            "n_threads": 2,
+            "memory_per_thread": memory_per_thread
+        }
 
-    #     client.shutdown()
+        client.shutdown()
 
 
-    # def test_client_dask_dir(self):
-    #     """
-    #     Run astrohack_local_client with N cores and with a memory_limit of M GB to create an instance of the
-    #     astrohack Dask client. Check that temporary files are written to dask_local_dir.
-    #     """
+    def test_client_dask_dir(self):
+        """
+        Run astrohack_local_client with N cores and with a memory_limit of M GB to create an instance of the
+        astrohack Dask client. Check that temporary files are written to dask_local_dir.
+        """
 
-    #     try:
+        try:
 
-    #         path = pathlib.Path(".").cwd() / "dask_test_dir"
+            path = pathlib.Path(".").cwd() / "dask_test_dir"
 
-    #         if path.exists() is False:
-    #             raise FileNotFoundError
+            if path.exists() is False:
+                raise FileNotFoundError
 
-    #     except FileNotFoundError:
-    #         assert False
+        except FileNotFoundError:
+            assert False
 
-    # def test_client_logger(self):
-    #     """
-    #     Run astrohack_local_client with N cores and with a memory_limit of M GB without any errors and the messages
-    #     will be logged in the terminal.
-    #     """
+    def test_client_logger(self):
+        """
+        Run astrohack_local_client with N cores and with a memory_limit of M GB without any errors and the messages
+        will be logged in the terminal.
+        """
 
-    #     files = os.listdir(".")
+        files = os.listdir(".")
 
-    #     try:
-    #         for file in files:
-    #             if re.match("^toolviper_log_file+[0-9].*log", file) is not None:
-    #                 return
+        try:
+            for file in files:
+                if re.match("^toolviper_log_file+[0-9].*log", file) is not None:
+                    return
 
-    #         raise FileNotFoundError
+            raise FileNotFoundError
 
-    #     except FileNotFoundError:
-    #         assert False
+        except FileNotFoundError:
+            assert False
