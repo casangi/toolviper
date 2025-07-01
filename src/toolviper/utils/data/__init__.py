c09f791a870be504385dcd2ecbefd3c5814698f3
Joshua Hoskins <jrh4dy@gmail.com>
2025-07-01 17:47:52 -0400

    Update __init__.py

--- src/toolviper/utils/data/__init__.py
+++ src/toolviper/utils/data/__init__.py
@@ -1,4 +1,4 @@
-#from .download import download, version, list_files, get_files, update
-from .cloudflare import download, version, list_files, get_files, update
+from .download import download, version, list_files, get_files, update
+#from .cloudflare import download, version, list_files, get_files, update
 
 __all__ = [s for s in dir() if not s.startswith("_")]
