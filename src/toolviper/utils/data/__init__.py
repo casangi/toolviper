aa9a9e0864b2570d69d501eb6ed41d5db4fbe6db
Joshua Hoskins <jrh4dy@gmail.com>
2025-07-17 15:50:56 -0400

    Add version import to __init__.py

--- src/toolviper/utils/data/__init__.py
+++ src/toolviper/utils/data/__init__.py
@@ -1,5 +1,5 @@
 from .download import dropbox
-from .cloudflare import download, list_files, get_files, update
+from .cloudflare import download, list_files, get_files, update, version
 
 
 __all__ = [s for s in dir() if not s.startswith("_")]
