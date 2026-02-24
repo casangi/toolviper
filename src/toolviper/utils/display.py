71935bcb126cde007d74102b23c73238ce2b0d07
jrhosk <joshua.hoskins@nao.ac.jp>
2026-02-24 15:52:51 +0900

    Add additional feature.

--- src/toolviper/utils/display.py
+++ src/toolviper/utils/display.py
@@ -1,15 +1,17 @@
 import re
 import operator
-
+from IPython.core.display import HTML
 
 def dict_to_html(d, indent=0):
+    print(f"THIS FUNCTION WILL BE DEPRECATED SOON")
 
     html = ""
     for key, value in d.items():
         if isinstance(value, dict):
             html += f"<div style='margin-left: {indent}em;'><details><summary>{key}</summary>{dict_to_html(value, indent + 1)}</details></div>"
         else:
-            html += f"<div style='margin-left: {indent}em;'><strong>{key}:</strong> {value}</div>"
+            html += f"<div style='margin-left: {indent}em;'><font color='blue'><strong>{key}:</strong></font> {value}</div>"
+
     return html
 
 
@@ -18,6 +20,9 @@ class DisplayDict(dict):
         super().__init__()
         self._dict = dictionary
 
+    def __repr__(self, *args, **kwargs):
+        return f"<class '{self.__class__.__name__}'>"
+
     @classmethod
     def from_dict(cls, dictionary):
         if isinstance(dictionary, dict):
@@ -25,32 +30,58 @@ class DisplayDict(dict):
 
         return None
 
-    def fetch(self, keys):
-        return list(operator.itemgetter(*keys)(self._dict))
+    @property
+    def data(self):
+        return self._dict
+
+    def select(self, keys, in_place=False):
+        _result = list(operator.itemgetter(*keys)(self._dict))
+
+        if in_place:
+            self._dict = _result
+
+        return DisplayDict.from_dict({key: value for key, value in zip(keys, _result)})
 
     def get_entries_(self, keys):
         return {key: value for key, value in self._dict.items() if key in keys}
 
-    def filter(self, query):
+    def filter(self, query, in_place=False):
+        _result = None
+
         if isinstance(query, list):
             _result = self.get_entries_(query)
-            return DisplayDict.from_dict(_result)
 
         if isinstance(query, str):
             _result = {
                 key: value for key, value in self._dict.items() if re.search(query, key)
             }
-            return self.from_dict(_result)
 
-        return None
+        if in_place:
+            self._dict = _result
+            return None
 
-    def display(self):
+        return DisplayDict.from_dict(_result)
+
+
+
+    def display(self, interactive=True):
         import rich
         from toolviper.utils.parameter import is_notebook
 
-        if is_notebook():
+        if is_notebook() and interactive:
             from IPython.display import JSON
 
             return JSON(self._dict)
 
         return rich.print_json(data=self._dict)
+
+    def html(self, indent=0):
+        html = ""
+        for key, value in self._dict.items():
+            if isinstance(value, dict):
+                html += f"<div style='margin-left: {indent}em;'><details><summary>{key}</summary>{dict_to_html(value, indent + 1)}</details></div>"
+
+            else:
+                html += f"<div style='margin-left: {indent}em;'><font color='blue'><strong>{key}:</strong></font> {value}</div>"
+
+        return HTML(html)
